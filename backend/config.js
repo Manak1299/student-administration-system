dff3098c42d0198e9dbf5f5f387830b1868d3769
Manak Agarwal <itsmemanak94@gmail.com>
2022-09-16 14:14:53 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://Manak:Manak1234@cluster0.4vxzj.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
+    MONGODB_URL: process.env.MONGODB_URL || 'test',
+    JWT_SECRET: process.env.JWT_SECRET || 'test'
 }
